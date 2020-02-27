c54e1bc40ea5d71cc4d64ecc06b1c2757cbb2056
klaa97 <10521251@polimi.it>
2020-02-27 18:56:13 +0100

    Add register method

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,14 +2,31 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from . import resources
+from flask_sqlalchemy import SQLAlchemy
+
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+api = Api(app)
+
+with app.app_context():
+    # Import resources
+    from . import resources
+    api.add_resource(resources.Index, '/')
+    api.add_resource(resources.Register, '/register')
+
+
 db.init_app(app)
 
-api = Api(app)
-api.add_resource(resources.Index, '/')
+
+@app.before_first_request
+def create_table():
+    db.drop_all()
+    db.create_all()
+
+
+
+
--- webapp/models.py
+++ webapp/models.py
@@ -4,7 +4,7 @@ from passlib.hash import pbkdf2_sha256 as sha256
 from . import db
 
 
-class UserModel(db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
@@ -15,8 +15,11 @@ class UserModel(db.Model):
     def generate_hash(password):
         return sha256.hash(password)
 
+    @classmethod
+    def exists_by_id(cls, searched_id):
+        return cls.query.filter_by(id=searched_id).count() != 0
 
-class ImageModel(db.Model):
+class Image(db.Model):
     __tablename__ = 'images'
     key = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
