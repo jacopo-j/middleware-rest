e354e472af8c2d9920c95d299343bb9d10785015
klaa97 <10521251@polimi.it>
2020-02-27 19:40:43 +0100

    Update schema, add various checks

--- webapp/resources.py
+++ webapp/resources.py
@@ -1,6 +1,9 @@
 from flask_restful import Resource, reqparse, Api
 from flask import make_response
+from sqlalchemy import func
+
 from .models import User, Image
+from webapp import db
 
 
 class Index(Resource):
@@ -13,20 +16,25 @@ class Index(Resource):
 class Register(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('id', help='This field cannot be blank', required=True)
+        parser.add_argument('username', help='This field cannot be blank', required=True)
         parser.add_argument('password', help='This field cannot be blank', required=True)
         data = parser.parse_args()
-        if User.exists_by_id(data['id']):
-            return {'message': 'User with id equal to {} already exists'.format(data['id'])}
-        new_user = User(id=data['id'],
+        if User.exists_by_username(data['username']):
+            return {'message': 'User with username equal to {} already exists'.format(data['username'])}
+        new_id = 0
+        if db.session.query(User).first():
+            new_id = db.session.query(func.max(User.id))
+        new_user = User(username=data['username'], id=new_id,
                         password=User.generate_hash(data['password']))
         try:
-            new_user.save_to_db()
+            db.session.add(new_user)
+            db.session.commit()
             # TODO JWT
             return {
                 'message': 'User {} was created'.format(data['username'])
             }
-        except:
+        except Exception as e:
+            print(e)
             return {'message': 'Internal error'}
 
 
