acc99a917d71871600244f0e88c1507b8eb332f0
klaa97 <10521251@polimi.it>
2020-02-28 15:19:15 +0100

    Add user retrieval functionality

--- webapp/models.py
+++ webapp/models.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import relationship
 from passlib.hash import pbkdf2_sha256 as sha256
 from . import db
 
-
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
