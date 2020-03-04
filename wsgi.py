94a23c78242ef4bd7358ac6fb29f48779e43f96c
Jacopo Jannone <jacopo-j@users.noreply.github.com>
2020-03-04 13:32:11 +0100

    Set heroku port

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@
 
 from webapp import create_app
 from webapp.modules import config
+from os import environ
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host=config['host'], port=config['default_port'])
+    app.run(host=config['host'], port=environ.get('PORT', config['default_port']))
